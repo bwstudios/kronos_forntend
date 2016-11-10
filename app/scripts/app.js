5cf11e3f8f4fe4181ab0513ba2c0db4db570cae5
Usuario <usuario@iMac-de-Usuario.local>
2016-11-10 15:01:36 -0500

    commit inicial con dependencias para template instaladas

---
+++ app/scripts/app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name kronosFrontendApp
+ * @description
+ * # kronosFrontendApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('kronosFrontendApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name kronosFrontendApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the kronosFrontendApp
+ */
+angular.module('kronosFrontendApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
